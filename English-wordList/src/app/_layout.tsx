5b9b4c318f3437be082262531391e484f372b9a9
Matheus Fábio <matheusfabiors@gmail.com>
2025-01-02 09:02:20 -0300

    feat: Get data Firestore

--- English-wordList/src/app/_layout.tsx
+++ English-wordList/src/app/_layout.tsx
@@ -5,9 +5,9 @@ import { SQLiteProvider } from "expo-sqlite";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false, statusBarStyle: "auto" }}>
-      <SQLiteProvider databaseName="myDatabase.db" onInit={initializeDatabase}>
-        <Stack.Screen name="(top-tabs)" />
-      </SQLiteProvider>
+      {/* <SQLiteProvider databaseName="myDatabase.db" onInit={initializeDatabase}> */}
+      <Stack.Screen name="(top-tabs)" />
+      {/* </SQLiteProvider> */}
     </Stack>
   );
 }
